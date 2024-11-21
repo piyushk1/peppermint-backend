f2ea78601c941b7f389130ac5ff6ee91e6f80944
Piyush Kukade <piyushkukade1@gmail.com>
2024-11-21 13:01:14 +0530

    API updated

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const Transaction = new mongoose.Schema({
-  amount: { type: Number, required: true },
-  description: { type: String, required: true },
+  type: { type: String, required: true },
   date: { type: Date, required: true },
-  type: { type: String, enum: ['Expense', 'Income'], required: true },
+  description: { type: String, required: true },
+  amount: { type: Number, required: true },
 });
 
 module.exports = mongoose.model('Transaction', Transaction);
