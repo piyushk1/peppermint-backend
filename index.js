6192b17993c571a778b4f7579243b2d508b16890
Piyush Kukade <piyushkukade1@gmail.com>
2024-11-21 14:01:17 +0530

    API Updated

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const transactionRoutes = require('./routes/transactionRoutes');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173', 
+  methods: 'GET, POST, PUT, DELETE', 
+  allowedHeaders: 'Content-Type, Authorization' 
+}));
 app.use(express.json()); 
 
 
